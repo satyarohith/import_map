a2c1fa5eaafdb3fc249a06ee109ca4fefac3848a
Satya Rohith <me@satyarohith.com>
2022-06-02 12:14:10 +0530

    import maps work

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "std/http/server.ts";
 
-serve((req: Request) => new Response("Hello World"));
+serve((req: Request) => new Response("Import maps work!"));
